c4f31bbd5118f0f4bd16f448d35721c8483d675c
Baz Murphy <bazmurphy@gmail.com>
2022-08-24 20:43:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = React.useState([])
+
+  useEffect(() => {
+    fetch("https://cyf-bazmurphy-cityguide.herokuapp.com")
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="heading">Mini Guide Client</h1>
+      <div className="city-select-container">
+        <label for="city" className="city-label">Choose A City:</label>
+        <select name="cities" id="city" className="city-select">
+          <option value="">Select A City</option>
+          <option value="stratford">Stratford</option>
+          <option value="heathrow">Heathrow</option>
+          <option value="harrow">Harrow</option>
+        </select>
+      </div>
+      <div className="category-buttons-container">
+        <button className="category-button">Pharmacies</button>
+        <button className="category-button">Colleges</button>
+        <button className="category-button">Hospitals</button>
+        <button className="category-button">Doctors</button>
+      </div>
+      <div className="data-container">
+
+      </div>
     </div>
   );
 }
